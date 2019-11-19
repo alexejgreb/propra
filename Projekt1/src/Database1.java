0ed9ab316479e79c4c830348e022bc0247c97f3d
Meike <meike.online@gmx.net>
2019-11-19 16:17:27 +0100

    add Ressources-Ordner

--- Projekt1/src/Database1.java
+++ Projekt1/src/Database1.java
@@ -12,7 +12,7 @@ public class Database1 {
 		public static Connection dbConnector(){
 		try{
 			Class.forName("org.sqlite.JDBC");
-			Connection con= DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Haith\\Desktop\\Projekt1\\Projekt1.db");
+			Connection con= DriverManager.getConnection("jdbc:sqlite:Projekt1.db");
 			System.out.println("SQLITE DB CONNECTED");
 			return con;
 		} catch(Exception e){
