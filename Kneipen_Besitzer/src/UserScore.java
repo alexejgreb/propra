ddf32b17aef95c1a51b2727e0fdaeee6caeb9571
Maxiv <MaxProPra>
2019-12-03 16:11:56 +0100

    .

--- Kneipen_Besitzer/src/UserScore.java
+++ Kneipen_Besitzer/src/UserScore.java
@@ -1,6 +1,11 @@
+import java.sql.Connection;
+
 public class UserScore {
 
     public void getUserScore(){
+        String password_hash = hashString(password);
+        Connection con = DataBaseConnector.dbConnectorMariaDB();
+        String s = "SELECT* ID_Nummer, Punkte FROM Kunden_Spiel WEHER Spiel_Nr = "+907;
 
     }
 }
