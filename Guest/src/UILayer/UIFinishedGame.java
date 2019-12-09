fc08a1d9b04366c668d80463c670534970559519
Maxiv <MaxProPra>
2019-12-09 16:03:49 +0100

    Bugfix

--- Guest/src/UILayer/UIFinishedGame.java
+++ Guest/src/UILayer/UIFinishedGame.java
@@ -1,17 +1,21 @@
 package UILayer;
 
+import DataLayer.DataBaseConnector;
 import LogicLayer.Login;
 
 import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.xml.crypto.Data;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UIFinishedGame extends JFrame {
 
     private JPanel contentPane;
+    private JPanel northPanel;
+    private JPanel southPanel;
     /**
      * Launch the application.
      */
@@ -44,16 +48,21 @@ public class UIFinishedGame extends JFrame {
     public UIFinishedGame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 300, 400);
-        contentPane = new JPanel();
+        contentPane = new JPanel(new BorderLayout());
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
-        contentPane.setLayout(null);
 
+
+        northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        contentPane.add(northPanel, BorderLayout.NORTH);
+
+        southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        contentPane.add(southPanel,BorderLayout.SOUTH);
 
         JLabel lblHeader = new JLabel("Ergebnis");
         lblHeader.setFont(new Font("Tahoma", Font.BOLD, 15));
         lblHeader.setBounds(5, 5, 424, 14);
-        contentPane.add(lblHeader);
+        northPanel.add(lblHeader);
 
         //Benutzername, bleibt leer wenn als Gast gespielt wird
         JLabel LabelUserName = new JLabel(""+ Login.guest.getUserName());
@@ -61,53 +70,60 @@ public class UIFinishedGame extends JFrame {
         contentPane.add(LabelUserName);
 
         //Spielnummer
-        JLabel LabelGameNr = new JLabel("Spiel Nr: "+ Login.guest.getGameNumber());
-        LabelGameNr.setBounds(5, 157, 97, 14);
-        contentPane.add(LabelGameNr);
+        JLabel labelGameNr = new JLabel("Spiel Nr: "+ Login.guest.getGameNumber());
+        labelGameNr.setFont(new Font("Tahoma", Font.BOLD, 15));
+        labelGameNr.setBounds(5, 157, 97, 14);
+        contentPane.add(labelGameNr);
 
         //Punkte
-        JLabel LabelPoints = new JLabel("Punkte: "+ Login.guest.getPoints());
-        LabelPoints.setBounds(5, 126, 97, 14);
-        contentPane.add(LabelPoints);
+        JLabel labelPoints = new JLabel("Punkte: "+ Login.guest.getPoints());
+        labelPoints.setFont(new Font("Tahoma", Font.BOLD, 15));
+        labelPoints.setBounds(5, 126, 97, 14);
+        contentPane.add(labelPoints);
 
         //Platzierung
-        JLabel LabelUserPlacing = new JLabel("Platz: "+ Login.guest.getPlacing());
-        LabelUserPlacing.setBounds(5, 95, 97, 14);
-        contentPane.add(LabelUserPlacing);
+        JLabel labelUserPlacing = new JLabel("Platz: "+ Login.guest.getPlacing());
+        labelUserPlacing.setFont(new Font("Tahoma", Font.BOLD, 15));
+        labelUserPlacing.setBounds(5, 95, 97, 14);
+        contentPane.add(labelUserPlacing);
 
-        JButton buttonNewGame = new JButton("Neues Spiel");
-        buttonNewGame.setFont(new Font("Tahoma", Font.BOLD, 15));
-        buttonNewGame.setForeground(new Color(255, 0, 0));
-        buttonNewGame.setBounds(80, 210, 130, 25);
-        contentPane.add(buttonNewGame);
-        buttonNewGame.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-
-				if(Login.guest.getGuestID()==0) {
-					UIGuest.main(null);
-					dispose();
-				}
-				else {
-					UIUserDefault.main(null);
-					dispose();
-				}
-
-            }
-        });
 
         JButton buttonLogout = new JButton("Zurück");
         buttonLogout.setFont(new Font("Tahoma", Font.BOLD, 15));
         buttonLogout.setForeground(new Color(255, 0, 0));
         buttonLogout.setBounds(80, 250, 130, 25);
-        contentPane.add(buttonLogout);
+        southPanel.add(buttonLogout);
         buttonLogout.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent arg0) {
-                UIDefault.main(null);
+                UIDefault.main(DataBaseConnector.getArgs());
                 dispose();
 
             }
         });
+
+
+        JButton buttonNewGame = new JButton("Neues Spiel");
+        buttonNewGame.setFont(new Font("Tahoma", Font.BOLD, 15));
+        buttonNewGame.setForeground(new Color(255, 0, 0));
+        buttonNewGame.setBounds(80, 210, 130, 25);
+        southPanel.add(buttonNewGame);
+        buttonNewGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+
+				if(Login.guest.getGuestID()==0) {
+					UIGuest.main(DataBaseConnector.getArgs());
+					dispose();
+				}
+				else {
+					UIUserDefault.main(DataBaseConnector.getArgs());
+					dispose();
+				}
+
+            }
+        });
+
+
     }
 
 }
