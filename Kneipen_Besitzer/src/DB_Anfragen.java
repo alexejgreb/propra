b7ec8d8ec39a1b898f692d475152316a037148dc
Maxiv <MaxProPra>
2019-12-09 18:57:30 +0100

    Bugfix

--- Kneipen_Besitzer/src/DB_Anfragen.java
+++ Kneipen_Besitzer/src/DB_Anfragen.java
@@ -1,17 +1,13 @@
 import java.sql.Connection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
 import net.proteanit.sql.DbUtils;
-
 
 public class DB_Anfragen {
 
@@ -20,85 +16,62 @@ public class DB_Anfragen {
         con=DataBaseConnector.dbConnectorMariaDB();
     }
 
-
-
     public static void Bar_Registration(String Surname,String Firstname,String Street,String City,long post,long mobil,String mail,String Nickname,String Message,int Note,long Password){ //// Tabelle Fragen
 
         //con=Database.dbConnector();
         con = DataBaseConnector.dbConnectorMariaDB();
         try{
-
-
-
             String sql = "INSERT INTO Bar (Surname, First_Name, Street,City,Post,Mobil,Mail,Nickname,Message,Note,Password)VALUES (?,?,?,?,?,?,?,?,?,?,?)" ;
             PreparedStatement pst=con.prepareStatement(sql);
-
             pst.setString(1, Surname);
             pst.setString(2, Firstname);
             pst.setString(3, Street);
             pst.setString(4,City);
             pst.setLong(5, post);
             pst.setLong(6, mobil);
-
             pst.setString(7, mail);
             pst.setString(8, Nickname);
             pst.setString(9,Message);
             pst.setLong(10,Note);
             pst.setLong(11,Password);
-
             pst.execute();
             JOptionPane.showMessageDialog(null,"Save");
-
-
         } catch (Exception e0) {
             JOptionPane.showMessageDialog(null,"Ungültige Einträge !!!!!");
         }
     }
 
-
     public static void Login(String Nickname,JLabel t,int pp,JLabel Name){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select password from Bar where  Surname= '"+Nickname+"' or First_Name='"+Nickname+"' or Mail='"+Nickname+"' or Nickname='"+Nickname+"'";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
             {
-
                 String password = rs.getString("Password");
                 t.setText(password);
                 pp=20;
                 Name.setText(Nickname);
-
             }else{
                 JOptionPane.showMessageDialog(null,"Prüfen sie ihren Nickname !!!!!");
             }
-
             pst1.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
 
-
     public static void PWD_Update1(String Nickname,JLabel t,int p){ //// Combo_Box Fragen_Nr
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select Note from Bar where Surname='"+Nickname+"' or First_Name='"+Nickname+"' or Mail='"+Nickname+"' or Nickname='"+Nickname+"' ";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
@@ -106,16 +79,11 @@ public class DB_Anfragen {
                 p=10;
                 t.setText(rs.getString("Note"));
             }
-
             pst1.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
-
     }
-
 
     public static void Update_PWD(int pwd1, int BarNr){
 
@@ -123,48 +91,33 @@ public class DB_Anfragen {
         con = DataBaseConnector.dbConnectorMariaDB();
 
         try{
-
             int values114=BarNr;
-
             int Values116=pwd1;
             String sql22="update Bar set Password= '"+values114+"',Note=0 where  Bar_Nr = '"+Values116+"' ";
             PreparedStatement pst3=con.prepareStatement(sql22);
             pst3.execute();
             JOptionPane.showMessageDialog(null,"Erfolgreich geändert");
-
-
-
         }catch(Exception e3){
             JOptionPane.showMessageDialog(null,e3);
         }
-
-
     }
-
 
     public static void Select_BarNr(String Nickname,JLabel t1){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select Bar_Nr from Bar where  Surname= '"+Nickname+"' or First_Name='"+Nickname+"' or Mail='"+Nickname+"' or Nickname='"+Nickname+"'";
-
             PreparedStatement pst111=con.prepareStatement(query);
-
             ResultSet rs= pst111.executeQuery();
 
             if ((rs.next()))
             {
-
                 String BarNr = rs.getString("Bar_Nr");
                 t1.setText(BarNr);
                 //pp=20;
-
             }
             pst111.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
@@ -173,31 +126,23 @@ public class DB_Anfragen {
     public static void Select_BarNr1(String Nickname1,JTextField t11){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select Bar_Nr from Bar where  Surname= '"+Nickname1+"' or First_Name='"+Nickname1+"' or Mail='"+Nickname1+"' or Nickname='"+Nickname1+"'";
-
             PreparedStatement pst111=con.prepareStatement(query);
-
             ResultSet rs= pst111.executeQuery();
 
             if ((rs.next()))
             {
-
                 String BarNr = rs.getString("Bar_Nr");
                 t11.setText(BarNr);
                 //pp=20;
-
             }
             pst111.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
 
     public static void Select_Password(String Nickname2,JLabel t2){ ////
 
@@ -206,28 +151,19 @@ public class DB_Anfragen {
         //con=Database.dbConnector();
         try {	 ///
             String query="select password from Bar where  Surname= '"+Nickname2+"' or First_Name='"+Nickname2+"' or Mail='"+Nickname2+"' or Nickname='"+Nickname2+"'";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
             {
-
                 String password = rs.getString("Password");
                 t2.setText(password);
-
-
-
-
                 pst1.close();
-
             }
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
 
     public static void Select_Password_Bar_Nr(String Nickname2,JLabel t2){ ////
 
@@ -236,58 +172,40 @@ public class DB_Anfragen {
         //con=Database.dbConnector();
         try {	 ///
             String query="select password from Bar where  Bar_Nr='"+Nickname2+"'";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
             {
-
                 String password = rs.getString("Password");
                 t2.setText(password);
-
-
-
-
                 pst1.close();
-
             }
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
 
     public static void Passwort_Neu(String Nickname1,JLabel t1){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select Bar_Nr from Bar where Mail='"+Nickname1+"'";
-
             PreparedStatement pst111=con.prepareStatement(query);
-
             ResultSet rs= pst111.executeQuery();
 
             if ((rs.next()))
             {
-
                 String BarNr = rs.getString("Bar_Nr");
                 t1.setText(BarNr);
                 //pp=20;
-
             }
             pst111.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
-
 
     public static void Update_Pass_Neu(long pwd1, long BarNr){
 
@@ -295,22 +213,15 @@ public class DB_Anfragen {
         con = DataBaseConnector.dbConnectorMariaDB();
 
         try{
-
             long Values116=BarNr;
-
             long Values114=pwd1;
             String sql22="update Bar set Password= '"+Values114+"',Note=1 where  Bar_Nr = '"+Values116+"' ";
             PreparedStatement pst3=con.prepareStatement(sql22);
             pst3.execute();
             JOptionPane.showMessageDialog(null,"Neue Password wurde erstellt !!!");
-
-
-
         }catch(Exception e3){
             JOptionPane.showMessageDialog(null,e3);
         }
-
-
     }
 
     public static void Max_BarNR(JTextField t){ ////
@@ -320,68 +231,40 @@ public class DB_Anfragen {
         //con=Database.dbConnector();
         try {	 ///
             String query="select max(Bar_Nr) from Bar";
-
             PreparedStatement pst111=con.prepareStatement(query);
-
             ResultSet rs= pst111.executeQuery();
 
             if ((rs.next()))
             {
-
                 String BarNr = rs.getString("max(Bar_Nr)");
-
-
-
-
-
                 t.setText(BarNr);
                 //pp=20;
-
             }
             pst111.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
-
-
     public static void Neu_Anmeldung(String BarNr ,JTextField t){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
-
         //con=Database.dbConnector();
         try {	 ///
             String query="select Note from Bar where Bar_Nr ='"+BarNr+"' ";
-
             PreparedStatement pst111=con.prepareStatement(query);
-
             ResultSet rs= pst111.executeQuery();
 
             if ((rs.next()))
             {
-
                 String Note = rs.getString("Note");
-
-
-
-
-
                 t.setText(Note);
                 //pp=20;
-
             }
             pst111.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
     }
-
-
 
     public static void Insert_Frage(String Frage,String ErsteWahl,String ZweiteWahl,String DritteWahl,String VierteWahl,String RichtigeAntwort,int Besitzer,JTextArea ta,JLabel Zeit){ ////
 
@@ -392,63 +275,36 @@ public class DB_Anfragen {
             pst.setString(1, Frage);
             pst.setString(2, ErsteWahl);
             pst.setString(3, ZweiteWahl);
-
             pst.setString(4, DritteWahl);
             pst.setString(5, VierteWahl);
             pst.setString(6, RichtigeAntwort);
             pst.setLong(7, Besitzer);
-
             pst.execute();
             JOptionPane.showMessageDialog(null,"Gespeichert");
-
-
-
-
-
-
         }
         catch(Exception e11){
             JOptionPane.showMessageDialog(null,"Prüfen sie die Angegebene Daten Bitte !!!");
         }
-
-
-
         ta.append("\t Frage Hinzugefügt: \n\n"+"Frage:\t"+Frage+"\n============================================================================\n"+"A:\t"+ErsteWahl+"\n============================================================================\n"+"B:\t"+ZweiteWahl+"\n============================================================================\n"+"C:\t"+DritteWahl+"\n============================================================================\n"+"D:\t"+VierteWahl+""+
                 " \t"+"Antwort:\t"+RichtigeAntwort+" \n============================================================================\n"+" \n============================================================================\n"+Zeit.getText()+"\n============================================================================\n"+"\n============================================================================\n"+"\n============================================================================\n");
-
-
     }
-
-
-
 
     public static void Delete_Frage(int FrageNr,int Besitzer,JTextArea ta,JLabel Zeit){ ////
 
         con = DataBaseConnector.dbConnectorMariaDB();
         try{
-
-
             String Delete =" delete from FragenPool where FrageNr = '"+FrageNr+"' and Besitzer ='"+Besitzer+"'";
 
             PreparedStatement pst3=con.prepareStatement(Delete);
             pst3.execute();
             pst3.close();
             JOptionPane.showMessageDialog(null,"die Frage '"+FrageNr+"' ist entfernt worden!!!");
-
-
         }
         catch(Exception e11){
             JOptionPane.showMessageDialog(null,"Prüfen sie die Angegebene Daten Bitte !!!");
         }
-
-
-
         ta.append("\t Frage Delete: \n\n"+"FrageNr:\t"+FrageNr+" \t"+"\n============================================================================\n"+" \n============================================================================\n"+Zeit.getText()+"\n============================================================================\n"+"\n============================================================================\n"+"\n============================================================================\n");
-
-
     }
-
-
 
     public static void Select_FrageNr(int BarNr ,JComboBox c){ ////
 
@@ -457,19 +313,14 @@ public class DB_Anfragen {
         //con=Database.dbConnector();
         try {	 ///
             String query="select FrageNr from FragenPool where Besitzer ='"+BarNr+"'";
-
             PreparedStatement pst=con.prepareStatement(query);
-
             ResultSet rs= pst.executeQuery();
 
             while (rs.next()){
                 String FrageNr = rs.getString("FrageNr");
                 c.addItem(FrageNr);
-
             }
             pst.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
@@ -483,14 +334,11 @@ public class DB_Anfragen {
         try {	 ///
 
             String query="select  Frage ,ErsteWahl ,ZweiteWahl,DritteWahl,VierteWahl,RichtigeAntwort from FragenPool WHERE FrageNr= '"+NR+"' ";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
             {
-
                 txt1.setText(rs.getString("Frage"));
                 txt2.setText(rs.getString("RichtigeAntwort"));
                 txt3.setText(rs.getString("ErsteWahl"));
@@ -498,15 +346,10 @@ public class DB_Anfragen {
                 txt5.setText(rs.getString("DritteWahl"));
                 txt6.setText(rs.getString("VierteWahl"));
             }
-
             pst1.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
-
-
     }
 
     public static void Select_Frage_FragenPool(int BarNr ,JComboBox c){ ////
@@ -516,9 +359,7 @@ public class DB_Anfragen {
         //con=Database.dbConnector();
         try {	 ///
             String query="select Frage from FragenPool where Besitzer ='"+BarNr+"'";
-
             PreparedStatement pst=con.prepareStatement(query);
-
             ResultSet rs= pst.executeQuery();
 
             while (rs.next()){
@@ -527,8 +368,6 @@ public class DB_Anfragen {
 
             }
             pst.close();
-
-
         } catch (Exception e3) {
             e3.printStackTrace();
         }
@@ -542,14 +381,11 @@ public class DB_Anfragen {
         try {	 ///
 
             String query="select  Frage ,ErsteWahl ,ZweiteWahl,DritteWahl,VierteWahl,RichtigeAntwort from FragenPool WHERE Frage= '"+F+"' ";
-
             PreparedStatement pst1=con.prepareStatement(query);
-
             ResultSet rs= pst1.executeQuery();
 
             if ((rs.next()))
             {
-
                 txt1.setText(rs.getString("Frage"));
                 txt2.setText(rs.getString("RichtigeAntwort"));
                 txt3.setText(rs.getString("ErsteWahl"));
